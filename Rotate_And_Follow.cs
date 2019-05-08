f94c49f78748b360d753b81444544ba2fffe3539
Mungari <48958397+Mungari@users.noreply.github.com>
2019-05-08 17:13:10 +0200

    Script per il tagalong + rotazione del menu
    Purtroppo non riesco a farlo centrato, aspetto che vengano i mitici tizi di hololens per chiedergli una mano in quel frangente. Nemmeno quello di microsoft, quello base che da il toolkit, riesce a seguire il gaze al 100% ma rimane "in disparte"

---
+++ Rotate_And_Follow.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate_And_Follow : MonoBehaviour
+{
+    public float Radius = 1f;
+    public float Speed = 2.0f;
+
+    private Vector3 perfectPosition;
+    private Vector3 targetPosition;
+    private float initialDistanceToCamera;
+    private bool displayRadius = false;
+    private bool displayTargetPosition = false;
+
+    void Start()
+    {
+        initialDistanceToCamera = Vector3.Distance(this.transform.position, Camera.main.transform.position);
+    }
+
+    void Update()
+    {
+        displayRadius = true; // display gizmos as soon as we hit play
+        Vector3 currentPos = this.transform.position;
+        perfectPosition = Camera.main.transform.position + Camera.main.transform.forward * initialDistanceToCamera;
+
+        Vector3 offsetDir = currentPos - perfectPosition;
+        displayTargetPosition = (offsetDir.magnitude > Radius);
+
+        if (displayTargetPosition)
+        {
+            targetPosition = perfectPosition + offsetDir.normalized *Radius;
+            this.transform.position = Vector3.Lerp(currentPos, targetPosition, Speed * Time.deltaTime);
+            this.transform.LookAt(Camera.main.transform);
+            this.transform.localRotation *= Quaternion.Euler(-0.5f, 130, 0);
+        }
+    }
+
+    public void OnDrawGizmos()
+    {
+        Color oldColor = Gizmos.color;
+
+        if (displayRadius)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(perfectPosition, Radius);
+        }
+
+        if (displayTargetPosition)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(targetPosition, 0.1f);
+        }
+
+        Gizmos.color = oldColor;
+    }
+}
